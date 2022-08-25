961da1d41b0ebf6a90702531a1e52c1c9cd5eef4
猴子 <a102009102009@gmail.com>
2022-08-25 08:55:18 +0800

    feat docker

--- src/getSchoolLatestNews.ts
+++ src/getSchoolLatestNews.ts
@@ -30,16 +30,18 @@ export const getSchoolLatestNew = async () => {
 
   try {
     const data = <string[]>(
-      JSON.parse(fs.readFileSync("catch-news.json", { encoding: "utf8" }))
+      JSON.parse(fs.readFileSync("catch/catch-news.json", { encoding: "utf8" }))
     );
 
     newNews = news.filter((news) => !data.includes(news.href));
     // eslint-disable-next-line no-empty
   } catch {}
 
-  fs.writeFileSync("catch-news.json", JSON.stringify(news.map((_) => _.href)), {
-    encoding: "utf8",
-  });
+  fs.writeFileSync(
+    "catch/catch-news.json",
+    JSON.stringify(news.map((_) => _.href)),
+    { encoding: "utf8" }
+  );
 
   return newNews;
 };
---
+++ start.dockerfile
@@ -0,0 +1,17 @@
+FROM node:16-alpine AS builder
+
+COPY ["./package.json", "./yarn.lock", "./"]
+
+RUN yarn --production --silent
+
+# app
+FROM node:16-alpine
+
+WORKDIR /app
+COPY --from=builder /node_modules/ /app/node_modules/
+COPY ./src ./src
+COPY --from=builder /package.json ./package.json
+
+VOLUME /catch /app/catch
+
+CMD yarn run start
